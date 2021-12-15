9f3d1ab7781940e031d55b52a43f520a17bf5a14
varjour <blankman7766@gmail.com>
2021-12-16 02:25:49 +0700

    making the subscription page

---
+++ resources/views/php/subscribe.blade.php
@@ -0,0 +1,66 @@
+<span class="login100-form-title p-b-26">
+						<img src="{{asset('assets/login/images/img/Logo_Vidio.png')}}"  width="195" height="120">
+<img src="https://static-web-prod-vidio.akamaized.net/assets/banners/premier-banner-desktop-19082021-2e57d2ad7dfe84358024c3c2fb9071e4db4355bc5a2ba63b23873f8b54768d1e.jpg"  width="1300" height="450" >
+<img src="{{asset('assets/login/images/img/mengapa.png')}}"   >
+<h2 id="title">Pilih paket yang sesuai untuk Kamu </h2> 
+<link rel="stylesheet" href="css/sub.css">
+<div class="hover-table-layout">
+    <div class="listing-item">
+        <figure class="image">
+            <img src="https://media.istockphoto.com/photos/metallic-gold-background-image-the-light-reflected-on-the-gold-foil-picture-id1329350166?b=1&k=20&m=1329350166&s=170667a&w=0&h=hBm5vtnUB3edJWhIAm4ytC80LQg3K1Fd7-PpxFKkH1w=" alt="image">
+            <figcaption>
+              <div class="caption">
+                <h1>Rp 19.000/Bulan</h1>
+                <p>Gold</p>
+                </div>
+            </figcaption>
+        </figure>
+        <div class="listing">
+            <h4>Tersedia di partner kami</h4>
+            <h4>Akses semua pertandingan Liga 1</h4>
+            <h4>Streaming TV seperti Fashion TV, Al Jazeera dll/h4>
+            <h4>*1 akun hanya bisa menyaksikan pada 1 perangkat saja</h4>
+        </div>
+    </div>
+    <div class="listing-item">
+        <figure class="image">
+            <img src="https://media.istockphoto.com/vectors/brushed-metal-background-vector-id619048186?b=1&k=20&m=619048186&s=612x612&w=0&h=raQe_nKXjmYyX2Bp1yRYpCdRn1nfCwN_7TT1fE8rUAk=" alt="image">
+            <figcaption>
+               <div class="caption">
+                <h1>Rp 29.000/Bulan</h1>
+                <p>Platinum</p>
+                </div>
+            </figcaption>
+        </figure>
+        <div class="listing">
+            <h4>Bebas nonton Copa America & Piala Walikota</h4>
+            <h4>Nonton bebas iklan video (pre-roll dan mid-roll)</h4>
+            <h4>Serigala Terakhir (Vidio Original Series)</h4>
+            <h4>Tayangan Olahraga, seperti: UCL,NBA, BRI Liga 1, Liga 2, Bein (1 & 2), BWF, WTA, Sudirman Cup dll</h4>
+            <h4>Serial dan Film, seperti: Vidio Original Series, Korea, Indonesia, dll</h4>
+            <h4>TV International, seperti: TVN, Zoo Moo,Zee Bioskop, dll</h4>
+        </div>
+    </div>
+    <div class="listing-item">
+        <figure class="image">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWq7JsnDTPRnXh_9RzhivvSPBcxS6n-fMJQFaDllPLWae9aFU4hvYRUopgicL2wFyZPyI&usqp=CAU" alt="image">
+            <figcaption>
+               <div class="caption">
+                <h1>Rp 49.000/Bulan</h1>
+                <p>Platinum+F1</p>
+                </div>
+            </figcaption>
+        </figure>
+        <div class="listing">
+            <h4>Termasuk seluruh konten Platinum</h4>
+            <h4>Tayangan eksklusif Formula 1 (F1) 2021</h4>
+            <h4>Nonton bebas iklan video (pre-roll dan mid-roll)</h4>
+            <h4>Serigala Terakhir (Vidio Original Series)</h4>
+            <h4>Tayangan Olahraga, seperti: UCL,NBA, BRI Liga 1, Liga 2, Bein (1 & 2), BWF, WTA, Sudirman Cup dll</h4>
+            <h4>Serial dan Film, seperti: Vidio Original Series, Korea, Indonesia, dll</h4>
+            <h4>TV International, seperti: TVN, Zoo Moo,Zee Bioskop, dll</h4>
+            
+            <img src="{{asset('assets/login/images/img/mengapa.png')}}"   >
+        </div>
+    </div>
+</div>
